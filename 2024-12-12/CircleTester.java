d1e6f723180369b81a54b316bf009b7562bb9b42
PrabinKuSabat <prabinkusabat@gmail.com>
2024-12-17 10:11:45 +0530

    Commit on Circle class

--- 2024-12-12/CircleTester.java
+++ 2024-12-12/CircleTester.java
@@ -1,11 +1,11 @@
 public class CircleTester {
     public static void main(String[] args){
-        //final char uInput;
+        char uInput;
         //do{
         Circle c1 = new Circle();
         System.out.println("Circumference: " + c1.getFormattedCircumference());
         System.out.println("Area: " + c1.getFormattedArea());
-        //uInput = System.
+        uInput =  
         //}while( uInput == 'y');
 
     }
